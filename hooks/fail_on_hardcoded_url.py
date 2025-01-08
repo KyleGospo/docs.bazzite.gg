0c62485f7fc804298980f62026a9907cae2bc632
Zeglius <33781398+Zeglius@users.noreply.github.com>
2025-01-08 18:12:21 +0100

    fix: Update hardcoded URL detection to use the line directly in error reporting

--- hooks/fail_on_hardcoded_url.py
+++ hooks/fail_on_hardcoded_url.py
@@ -1,12 +1,13 @@
 import re
-import textwrap
+
+from mkdocs import plugins
 from mkdocs.config.defaults import MkDocsConfig
 from mkdocs.structure.pages import Page
-
 
 pattern = r"\(https?:\/\/docs\.bazzite\.gg\/"
 
 
+@plugins.event_priority(100)
 def on_page_markdown(markdown: str, page: Page, config: MkDocsConfig, files):
     for line_n, line in enumerate(markdown.splitlines()):
         if match := re.search(pattern, line):
@@ -14,5 +15,5 @@ def on_page_markdown(markdown: str, page: Page, config: MkDocsConfig, files):
                 "Hardcoded URL found in markdown: ",
                 page.file,
                 f"line {line_n}:{match.start()}",
-                textwrap.TextWrapper().wrap(line),
+                line,
             )
