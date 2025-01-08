96b761d7c452792e8f0a588b72c285d64f5d1a6a
Zeglius <33781398+Zeglius@users.noreply.github.com>
2025-01-08 18:05:50 +0100

    fix: Improve error reporting for hardcoded URL detection in markdown

--- hooks/fail_on_hardcoded_url.py
+++ hooks/fail_on_hardcoded_url.py
@@ -1,4 +1,5 @@
 import re
+import textwrap
 from mkdocs.config.defaults import MkDocsConfig
 from mkdocs.structure.pages import Page
 
@@ -7,5 +8,11 @@ pattern = r"[^[]\(https?:\/\/docs\.bazzite\.gg"
 
 
 def on_page_markdown(markdown: str, page: Page, config: MkDocsConfig, files):
-    if (_match := re.search(pattern, markdown)) is not None:
-        raise ValueError("Hardcoded URL found in markdown: ", page.file, _match.group())
+    for line_n, line in enumerate(markdown.splitlines()):
+        if match := re.search(pattern, line):
+            raise ValueError(
+                "Hardcoded URL found in markdown: ",
+                page.file,
+                f"line {line_n}:{match.start()}",
+                textwrap.TextWrapper().wrap(line),
+            )
