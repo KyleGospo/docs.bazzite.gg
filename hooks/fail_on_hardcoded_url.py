c8997ddb1b38b286dede2fc7acd0e8f18ad8803d
Zeglius <33781398+Zeglius@users.noreply.github.com>
2025-01-08 17:56:23 +0100

    fix: Enhance error message for hardcoded URL detection in markdown

--- hooks/fail_on_hardcoded_url.py
+++ hooks/fail_on_hardcoded_url.py
@@ -7,5 +7,5 @@ pattern = r"[^[]\(https?:\/\/docs\.bazzite\.gg"
 
 
 def on_page_markdown(markdown: str, page: Page, config: MkDocsConfig, files):
-    if _match := re.search(pattern, markdown):
-        raise ValueError("Hardcoded URL found in markdown: ", page.file)
+    if (_match := re.search(pattern, markdown)) is not None:
+        raise ValueError("Hardcoded URL found in markdown: ", page.file, _match.group())
